7dc1ec41e88e23d282c6b6de4d8be3b5dc0f7e91
Gabriela <aragao.gabi@gmail.com>
2021-06-17 15:41:04 -0300

    Versão final

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace SBF_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Digite o valor do produto em reais: R$");
+            double produto = double.Parse(Console.ReadLine());
+
+            double convertDolar = (produto/5.06);
+            double convertEuro =  (produto/6.10);
+            double convertIndian = (produto*14.76);
+
+            Console.WriteLine("O valor em Dólar é ${0:00} ", convertDolar);
+            Console.WriteLine("O valor em Euro é {0:00}", convertEuro);
+            Console.WriteLine("O valor em Indian Rupee é {0:00}", convertIndian);
+
+
         }
     }
 }
